c78ea45bac31fb0bf5ccb69353eceabf80407403
Engin Karataş <enginkaratas99@gmail.com>
2021-01-30 18:28:26 +0300

    youtube video
    some changes for youtube

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express')
 var app = express();
 const port = 3000
 app.listen(port,() => console.log("listening : "+ port))
+
+//NUNJUCKS
+
 const nunjucks = require('nunjucks')
 
 nunjucks.configure('views', {
@@ -9,18 +12,60 @@ nunjucks.configure('views', {
     express: app
 });
 
-app.get('/hello/:name', function(req, res) {
-    res.render('hello.html',{
-      name: req.params.name
-    });
-});
-
-app.get('/',function(req,res){
+app.get('/api/:isim',(req,res) => {
   res.render('hello.html',{
-    
-    
-    sorular:[{ title: "foo", id: 1 }, { title: "bar", id: 2}] 
-
-  
+    name: req.params.isim,
+    number:"343"
   })
 })
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// app.get('/hello/:name', function(req, res) {
+//     res.render('hello.html',{
+//       name: req.params.name
+//     });
+// });
+
+// app.get('/',function(req,res){
+//   res.render('hello.html',{
+    
+//     sorular:[{ title: "foo", id: 1 }, { title: "bar", id: 2}] 
+  
+//   })
+// })
+
+
+
+
+
+
+
+
+
+
+
+{/* <h1>Posts</h1>
+<ul>
+{% for item in sorular %}
+  <li>{{ item.title }}</li>
+{% else %}
+  <li>This would display if the 'item' collection were empty</li>
+{% endfor %}
+</ul> */}
